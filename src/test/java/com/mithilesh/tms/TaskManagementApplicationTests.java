add2898a9c86704ea7aef33e62f2418aba6fe771
mithilesh shah <mithileshkshah@gmail.com>
2023-04-04 22:52:31 +0530

    initial commit

---
+++ src/test/java/com/mithilesh/tms/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mithilesh.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
